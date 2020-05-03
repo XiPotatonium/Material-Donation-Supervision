7385946cd7077a74db82aba5e7da0f700bb0541f
XI <xipotatonium@outlook.com>
2020-05-03 16:57:31 +0800

    登陆页面的UI和基本转跳

--- MDS.Client/LoginDialog.xaml.cs
+++ MDS.Client/LoginDialog.xaml.cs
@@ -17,9 +17,46 @@ namespace MDS.Client
     /// </summary>
     public partial class LoginDialog : Window
     {
+        enum LoginDialogMode
+        {
+            LOGIN, REGISTER
+        }
+
+        private LoginDialogMode Mode { set; get; } = LoginDialogMode.LOGIN;
+
         public LoginDialog()
         {
             InitializeComponent();
+        }
+
+        private void PrimaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void SwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (Mode)
+            {
+                case LoginDialogMode.LOGIN:
+                    Mode = LoginDialogMode.REGISTER;
+                    PrimaryButton.Content = "注册";
+                    SwitchButton.Content = "登陆";
+                    LoginRoot.Visibility = Visibility.Collapsed;
+                    RegisterRoot.Visibility = Visibility.Visible;
+                    break;
+                case LoginDialogMode.REGISTER:
+                    Mode = LoginDialogMode.LOGIN;
+                    PrimaryButton.Content = "登陆";
+                    SwitchButton.Content = "注册";
+                    LoginRoot.Visibility = Visibility.Visible;
+                    RegisterRoot.Visibility = Visibility.Collapsed;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
