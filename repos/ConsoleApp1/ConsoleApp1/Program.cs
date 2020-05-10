2ccba96cba3cea98fdb9ca6f5dc44d9828dc322c
mesutozil272 <50457939+mesutozil272@users.noreply.github.com>
2020-05-10 21:27:42 +0800

    网络初步
    App1为客户端用(post, get)，app2为服务端用（listener）

---
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //student requestData;
+            string requestData = "1234";                 // 发送的数据
+            //requestData.mts = 2;
+            //requestData.explain = "1234567";
+            
+            string url = "http://localhost:6666";  // 服务器地址
+
+            //string result = HttpGet(url, requestData);    // GET请求
+            string result = HttpPost(url, requestData);     // POST请求
+
+            Console.WriteLine("响应结果：" + result);
+            Console.ReadKey();
+        }
+
+        private static String HttpGet(string url, string requestData)
+        {
+            // 实例化请求对象
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "?" + requestData);
+            request.Method = "GET";
+            request.ContentType = "text/html; charset=UTF-8";
+
+            // 实例化响应对象，获取响应信息
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader sReader = new StreamReader(responseStream, Encoding.Default);
+            String result = sReader.ReadToEnd();
+            sReader.Close();
+            responseStream.Close();
+
+            return result;
+        }
+
+        private static String HttpPost(string url, string requestData)
+        {
+            // 实例化请求对象
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = requestData.Length;
+
+            // 发送请求数据
+            Stream requestStream = request.GetRequestStream();
+            StreamWriter sWriter = new StreamWriter(requestStream, Encoding.Default);
+            sWriter.Write(requestData);
+            sWriter.Flush();
+            sWriter.Close();
+            requestStream.Close();
+
+            // 实例化响应对象，获取响应信息
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader sReader = new StreamReader(responseStream, Encoding.Default);
+            String result = sReader.ReadToEnd();
+            sReader.Close();
+            responseStream.Close();
+
+            return result;
+        }
+    }
+}
---
+++ repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using System.IO;
+//https://docs.microsoft.com/zh-cn/dotnet/api/system.net.httplistener?view=netframework-4.7.2
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (HttpListener listener = new HttpListener())
+                {
+                    listener.Prefixes.Add("http://localhost:6666/");
+                    listener.Start();
+                    Console.WriteLine("开始监听");
+                    while (true)
+                    {
+                        try
+                        {
+                            HttpListenerContext context = listener.GetContext();//阻塞
+                            HttpListenerRequest request = context.Request;
+                            string postData = new StreamReader(request.InputStream).ReadToEnd();
+                            Console.WriteLine("收到请求：" + postData);
+                            HttpListenerResponse response = context.Response;//响应
+                            string responseBody = "响应";
+                            response.ContentLength64 = System.Text.Encoding.UTF8.GetByteCount(responseBody);
+                            response.ContentType = "text/html; Charset=UTF-8";
+                            //输出响应内容
+                            Stream output = response.OutputStream;
+                            using (StreamWriter sw = new StreamWriter(output))
+                            {
+                                sw.Write(responseBody);
+                            }
+                            Console.WriteLine("响应结束");
+                        }
+                        catch (Exception err)
+                        {
+                            Console.WriteLine(err.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("程序异常，请重新打开程序：" + err.Message);
+            }
+        }
+    }
+}
