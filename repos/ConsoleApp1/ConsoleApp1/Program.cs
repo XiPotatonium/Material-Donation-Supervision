60e7b5c4c43fd98cb38f6835ff818cdbdefbfb0b
mesutozil272 <50457939+mesutozil272@users.noreply.github.com>
2020-05-14 02:41:04 +0800

    将发送、接受的类型由字符串修改为对象

--- repos/ConsoleApp1/ConsoleApp1/Program.cs
+++ repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,28 +5,61 @@ using System.Text;
 using System.Net;
 using System.IO;
 using System.Runtime.InteropServices;
-
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //student requestData;
-            string requestData = "1234";                 // 发送的数据
-            //requestData.mts = 2;
-            //requestData.explain = "1234567";
+        { 
+            double rd = 12345;                 // 发送的数据
             
             string url = "http://localhost:6666";  // 服务器地址
-
+            //string requestData = objecttostring(rd);
             //string result = HttpGet(url, requestData);    // GET请求
-            string result = HttpPost(url, requestData);     // POST请求
+            object result = HttpPost(url, rd);     // POST请求
 
             Console.WriteLine("响应结果：" + result);
             Console.ReadKey();
         }
-
+        public static object DeserializeObject(byte[] bytes)
+        {
+            object obj = null;
+            if (bytes == null)
+                return obj;
+            MemoryStream ms = new MemoryStream(bytes);
+            ms.Position = 0;
+            BinaryFormatter formatter = new BinaryFormatter();
+            obj = formatter.Deserialize(ms);
+            ms.Close();
+            return obj;
+        }
+        public static byte[] HexStringToByteArray(string s)
+        {
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+        public static string objecttostring(object obj)
+        {
+            if (obj == null)
+                return null;
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(ms, obj);
+            ms.Position = 0;
+            byte[] bytes = new byte[ms.Length];
+            ms.Read(bytes, 0, bytes.Length);
+            ms.Close();
+            string inputString = null;
+            foreach (byte b in bytes)
+            {
+               inputString += b.ToString("X2") ;
+            }
+            return inputString;
+        }
         private static String HttpGet(string url, string requestData)
         {
             // 实例化请求对象
@@ -37,7 +70,7 @@ namespace ConsoleApplication1
             // 实例化响应对象，获取响应信息
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream responseStream = response.GetResponseStream();
-            StreamReader sReader = new StreamReader(responseStream, Encoding.Default);
+            StreamReader sReader = new StreamReader(responseStream, Encoding.UTF8);
             String result = sReader.ReadToEnd();
             sReader.Close();
             responseStream.Close();
@@ -45,13 +78,15 @@ namespace ConsoleApplication1
             return result;
         }
 
-        private static String HttpPost(string url, string requestData)
+        private static object HttpPost(string url, object rd)
         {
+            string requestData = objecttostring(rd);
+            Console.WriteLine(requestData.Length);
             // 实例化请求对象
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = requestData.Length;
+            request.ContentType = "text/html";
+            request.ContentLength = requestData.Length;// 2 *requestData.Length+4; //有没有比+8更好的办法？
 
             // 发送请求数据
             Stream requestStream = request.GetRequestStream();
@@ -65,10 +100,11 @@ namespace ConsoleApplication1
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream responseStream = response.GetResponseStream();
             StreamReader sReader = new StreamReader(responseStream, Encoding.Default);
-            String result = sReader.ReadToEnd();
+            string responsestr = sReader.ReadToEnd();
             sReader.Close();
             responseStream.Close();
-
+            byte[] rbytes = HexStringToByteArray(responsestr);
+            object result = DeserializeObject(rbytes);
             return result;
         }
     }
