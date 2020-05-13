3f0e61e5b6b04bf36e2cfa13ec068ba588064528
XI <xipotatonium@outlook.com>
2020-05-13 17:07:06 +0800

    申请页Stepper基本好了

--- MDS.Client/LoginDialog.xaml.cs
+++ MDS.Client/LoginDialog.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -29,7 +30,7 @@ namespace MDS.Client
             InitializeComponent();
         }
 
-        private void PrimaryButton_Click(object sender, RoutedEventArgs e)
+        private async void PrimaryButton_Click(object sender, RoutedEventArgs e)
         {
             // TODO 假数据
             UserInfo.Id = 0;
@@ -37,6 +38,9 @@ namespace MDS.Client
             UserInfo.PhoneNumber = "152-1111-1111";
             UserInfo.HomeAddress = "XX省-XX市-XX区-XX街道-XX小区-XXXXXXXXXXXXX";
             UserInfo.UserType = UserType.ADMIN;
+
+            PrimaryButton.IsEnabled = false;    // 防止重复按下
+            await Task.Delay(200);              // 模拟网络延迟
 
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
--- MDS.Client/NavigationPages/ApplicationPage.xaml.cs
+++ MDS.Client/NavigationPages/ApplicationPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace MDS.Client.NavigationPages
 {
@@ -53,14 +54,14 @@ namespace MDS.Client.NavigationPages
                 // TODO 网络请求ApplicationDetailViewModel
 
                 // TODO 根据ApplicationViewModel和ApplicationDetailViewModel来生成正确的Tab显示
-                PART_Stepper.SelectedIndex = 1;
+                PART_Stepper.Controller.GotoStep(1);
             }
             else
             {
                 // TODO 填写新申请，请求所有可选的物资
 
                 // TODO 初始化好填写界面
-                PART_Stepper.SelectedIndex = 0;
+                PART_Stepper.Controller.GotoStep(0);
             }
 
             RefreshApplicationCardView();
@@ -70,8 +71,9 @@ namespace MDS.Client.NavigationPages
 
         private void RefreshApplicationCardView()
         {
-            if (PART_Stepper.SelectedIndex != 0)
+            if (PART_Stepper.Controller.PreviousStep != null)
             {
+                // 不是第一步
                 PART_Card.Visibility = Visibility.Visible;
                 CardApplicationId.Text = ApplicationViewModel.GUID;
                 CardApplicationName.Text = ApplicationViewModel.Name;
@@ -82,6 +84,40 @@ namespace MDS.Client.NavigationPages
             {
                 PART_Card.Visibility = Visibility.Collapsed;
             }
+        }
+
+        private void AddressSettingButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void PART_Stepper_ContinueNavigation(object sender, MaterialDesignExtensions.Controls.StepperNavigationEventArgs args)
+        {
+            int idx = 0;
+            for (; idx < PART_Stepper.Controller.Steps.Length; ++idx)
+            {
+                if (args.CurrentStep == PART_Stepper.Controller.Steps[idx])
+                {
+                    break;
+                }
+            }
+            if (idx == 0)
+            {
+                // 从申请到等待审核
+                // TODO 这里有一个请求要发
+                await Task.Delay(100);
+                ApplicationViewModel = new ApplicationListViewModel();
+                ApplicationDetailViewModel = new ApplicationDetailViewModel();
+                RefreshApplicationCardView();
+            }
+            else if (idx == 3)
+            {
+                // 确认
+            }
+        }
+
+        private void PART_Stepper_CancelNavigation(object sender, MaterialDesignExtensions.Controls.StepperNavigationEventArgs args)
+        {
+            // TODO 这里需要发送撤销请求的包
         }
     }
 
