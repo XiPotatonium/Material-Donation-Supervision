c4910c7a4271bcfc76086afa6ced3d5f7f4b0925
flyfaerss <50093680+flyfaerss@users.noreply.github.com>
2020-06-04 23:18:21 +0800

    Add files via upload

---
+++ DTO/AdminData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class AdminData
+    {
+
+    }
+
+    public enum AdminState
+    {
+        FINISH, WAIT
+    }
+
+    public enum AdminResult
+    {
+        PASS, FAIL, NONE
+    }
+
+    public class Normal
+    {
+        public string Number { set; get; }
+        public string User { set; get; }
+        public DateTime Time { set; get; }
+        public AdminState State { set; get; }
+        public string Reviewer { set; get; }
+        public AdminResult Result { set; get; }
+        public string Content { set; get; }
+        public string Remarks { set; get; }
+    }
+
+    public class AutherRequestList
+    {
+        public List<Normal> a_normals { set; get; }
+    }
+
+    public class MaterialAuditList
+    {
+        public List<Normal> m_normals { set; get; }
+    }
+}
