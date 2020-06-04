064412dcbc2ce77279f5852d96e3d2af61338172
WangXiuhang <56787333+WangXiuhang@users.noreply.github.com>
2020-06-04 22:40:39 +0800

    Update 物资调配.sql

--- DemoServer/Service/物资调配.sql
+++ DemoServer/Service/物资调配.sql
@@ -1,30 +1,36 @@
-use ���ʵ���;
+use 物资调配;
 
-create table Users(    --�����û���
+create table Users(    --建立用户表
    PhoneNumber varchar(20) primary key,
    Passwords varchar(20) not null,
-   HomeAddress varchar(20) not null,
-   UserType varchar(20) constraint usertypes check (UserType = 'NORMAL' or 
-           UserType = 'ADMIN' or UserType = 'DELIVERER')
+   UserID int not null,
+   HomeAddress varchar(20),
+  -- UserType varchar(20) constraint usertypes check (UserType = 'NORMAL' or 
+    --       UserType = 'ADMIN' or UserType = 'DELIVERER')
 );
 
 
-create table Materials(   --�������ʱ�
-    --MaterialGUID varchar(10) primary key,
-    MaterialName varchar(20) primary key,
+create table Materials(   --建立物资表
+   
+	MaterialID int primary key,
+    MaterialName varchar(20) unique,
+
 	MaterialQuantity int,
-	MaterialDescription varchar(50)  
+	MaterialDescription varchar(80),
+	MaterialConstraint varchar(80)
 );
 
 
-create table DonationOrApplication(    --����������
-    DonateGUID varchar(10) primary key,
+create table Donation(    --建立捐赠表
+    DonateID int primary key,
+    DonateGUID varchar(10) unique,
 	DonatorName varchar(20),
 	DonatorAddress varchar(20),
 	MaterialName varchar(20),
 	MaterialQuantity int,
 	DonationState varchar(20) constraint donationstates check ( DonationState = 'Aborted' or DonationState = 'Applying' 
 	     or DonationState = 'WaitingDelivery' or DonationState = 'Done'),
+    StateIndex int,
     StartTime varchar(15),
 	--foreign key(DonatorAddress) references Users(HomeAddress) on delete cascade,
     foreign key(MaterialName) references Materials(MaterialName) on delete cascade,
@@ -32,23 +38,27 @@ create table DonationOrApplication(    --����������
 
     );
 
-create table Applications(         --���������
-  ApplyGUID varchar(10) primary key,
-	ApplierName varchar(20),
+create table Applications(         --建立申请表
+  ApplyID int primary key,
+  ApplyGUID varchar(10) unique,
+  ApplierId int,
+	--ApplierName varchar(20),
 	ApplierAddress varchar(20),
 	MaterialName varchar(20),
 	MaterialQuantity int,
 	ApplicationState varchar(20) constraint applicationstates check (ApplicationState = 'Aborted' or ApplicationState = 'Applying' 
 	     or ApplicationState = 'Delivering' or ApplicationState = 'Received' or ApplicationState = 'Done'),
+		 StateIndex int,
    StartTime varchar(15),
+  -- ApplierAddress varchar(30),
 
-   foreign key(MaterialName) references Materials(MaterialName) on delete cascade,
-	foreign key(ApplierName) references Users(PhoneNumber) on delete cascade
+  -- foreign key(MaterialName) references Materials(MaterialName) on delete cascade,
+	--foreign key(ApplierName) references Users(PhoneNumber) on delete cascade
 );
 
 
 
-create table Delivery(                 --������ݱ�
+create table Delivery(                 --建立快递表
     DeliveryGUID varchar(10) primary key,
 	DeliveryerName varchar(20),
 
@@ -65,114 +75,17 @@ create table Delivery(                 --������ݱ�
 
 );
 
+insert into Materials 
+values(30000,'84消毒液(500mL)',0,'84消毒液主要用于各种物体表面和环境的消毒，可有效杀灭病菌',' ');
 
+insert into Materials 
+values(30001,'医用酒精(500mL)',0,'医用酒精成份主要是乙醇，主要用于日常消毒，使病毒蛋白质失活，防止病毒扩散',' ');
 
+insert into Materials
+values(30002,'普通医用口罩',0,'普通医用口罩可以有效遮挡细菌、病毒等致病微生物的感染',' ');
 
+insert into Materials
+values(30003,'KN95口罩',0,'KN95口罩比普通医用口罩标准更加严格，对疫情防控更有效',' ');
 
-
-
---create table Applicants(
- --  names varchar(15) primary key,
- --  passwords varchar(15) not null,
- --  phoneNumber varchar(20) not null,
- --  addresses varchar(30)  not null,
- --  loginState varchar(10) not null constraint states check (loginState = 'online' or loginState = 'offline')
---);
-
---create table Donors(
---   names varchar(15) primary key,
---   passwords varchar(15) not null,
---   loginState varchar(10) not null constraint states check (loginState = 'online' or loginState = 'offline')
-
---);
---create table Admins(
---  names varchar(15) primary key,
---  passwords varchar(15) not null,
---  loginState varchar(10) not null constraint states check (loginState = 'online' or loginState = 'offline')
-
-
---);
---create table Distributors(
---   names varchar(15) primary key,
---   passwords varchar(15) not null,
---   phoneNumber varchar(20) not null,
---   loginState varchar(10) not null constraint states check (loginState = 'online' or loginState = 'offline')
-
-
---);
-
---create table Donate(
-  -- MaterialGUID varchar(10) primary key,
---  MaterialName varchar(20),
---  -- number varchar(10),
---   materialCount int,
-
---   StartID varchar(15),
-   
-
---   Departure varchar(20) not null,
---   Destination varchar(20) not null,
---   DonateState varchar(15) not null constraint donatestates check (DonateState = 'Waiting' or DonateState = 'Processing'
---                or DonateState = 'Finished'),
---   ApplicationState varchar(15) not null constraint applicatonstates check (ApplicationState = 'Aborted' or ApplicationState = 'Applying' 
---            or ApplicationState = 'Delivering' or ApplicationState = 'Received' or ApplicationState = 'Done'),
-   
---   StartTime varchar(20),
---   FinishTime varchar(20),
-
- 
---   foreign key(MaterialName) references Donors(names) on delete cascade,
---   foreign key(MaterialName) references Materials(MaterialName) on delete cascade
-
-
-
---);
-
---create table Applys(
---   names varchar(15) primary key,
---   number varchar(10),
---   materialCount int,
---   foreign key(names) references Applicants(names) on delete cascade,
---   foreign key(number) references Materials(number) on delete cascade
-
-
-
---);
-
---create table Distribute(
-   --name1 varchar(15) not null ,
-   --name2 varchar(15) not null,
-   --number varchar(10),
-  -- DistributeState varchar(20),
-   --foreign key(name1) references Distributors(names) on delete cascade,
-  -- foreign key(name2) references Applicants(names) on delete cascade,
-  -- foreign key(number) references Materials(number) on delete cascade
-
-
-
---);
---grant all previleges on Users
---to 
---grant all privileges
---on Materials
---to admins;
---grant all privileges
---on Applicants
---to admins;
---grant all privileges
---on Donors
---to admins;
---grant all privileges
---on Distributors
---to admins;
---grant all privileges 
---on Donate
---to admins;
---grant all privileges 
---on Applys
---to admins;
---grant all privileges 
---on Distribute
---to admins;
-
-
+insert into Materials
+values(30004,'护目镜',0,'长期接触易感人群者应佩戴护目镜，防止病菌进入眼睛',' ');
