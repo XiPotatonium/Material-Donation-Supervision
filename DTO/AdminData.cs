d153695df90761fc66c9af4fd11b907fdf1fd92e
flyfaerss <50093680+flyfaerss@users.noreply.github.com>
2020-06-05 22:30:58 +0800

    Add files via upload
    有关管理员的resquest和response

--- DTO/AdminData.cs
+++ DTO/AdminData.cs
@@ -25,19 +25,91 @@ namespace DTO
         public string User { set; get; }
         public DateTime Time { set; get; }
         public AdminState State { set; get; }
-        public string Reviewer { set; get; }
+        public int Reviewer { set; get; }
         public AdminResult Result { set; get; }
         public string Content { set; get; }
         public string Remarks { set; get; }
     }
 
-    public class AutherRequestList
+    [Serializable]
+    public class AutherRequestListRequest : IReturn<AutherRequestListResponse>
+    {
+        public int AdminID { set; get; }  //管理员ID
+        public AdminState state { set; get; }  //当前任务状态
+    }
+
+    [Serializable]
+    public class AutherRequestListResponse
     {
         public List<Normal> a_normals { set; get; }
     }
 
-    public class MaterialAuditList
+    [Serializable]
+    public class AutherRequestAgreeRequest : IReturn<AutherRequestAgreeResponse>
+    {
+        public int AdminID { set; get; } //管理员id
+        public string Number { set; get; } //申请编号
+        public string Secondary_passward { set; get; }
+    }
+
+    [Serializable]
+    public class AutherRequestAgreeResponse
+    {
+        public int flag { set; get; }
+    }
+
+    [Serializable]
+    public class AutherRequestRefuseRequest : IReturn<AutherRequestRefuseResponse>
+    {
+        public int AdminID { set; get; } //管理员id
+        public string Number { set; get; } //申请编号
+        public string Secondary_passward { set; get; }
+    }
+
+    [Serializable]
+    public class AutherRequestRefuseResponse
+    {
+        public int flag { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditListRequest : IReturn<MaterialAuditListResponse>
+    {
+        public int AdminID { set; get; }
+        public AdminState state { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditListResponse
     {
         public List<Normal> m_normals { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditAgreeRequest : IReturn<MaterialAuditAgreeResponse>
+    {
+        public int AdminID { set; get; } //管理员id
+        public string Number { set; get; } //申请编号
+        public string Secondary_passward { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditAgreeResponse
+    {
+        public int flag { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditRefuseRequest : IReturn<MaterialAuditRefuseResponse>
+    {
+        public int AdminID { set; get; } //管理员id
+        public string Number { set; get; } //申请编号
+        public string Secondary_passward { set; get; }
+    }
+
+    [Serializable]
+    public class MaterialAuditRefuseResponse
+    {
+        public int flag { set; get; }
     }
 }
